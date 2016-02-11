6fb988786eba8e243f8ae3e381cc191c611703eb
Wei Ren <victor-ren@outlook.com>
2016-02-11 15:37:07 -0600

    fixed return value

--- tasks.py
+++ tasks.py
@@ -1,4 +1,4 @@
-# import numpy as np
+import numpy as np
 # from astropy.io import fits
 
 
@@ -6,19 +6,19 @@
 # filename = "/www/static/images/image.fits"
 
 def tasks(param):
-    return ({"result": "success"}, None)
+    return ({"result": param}, None)
 
 # Simple task calculating average value in a region.
 # Boundary assumes the expected format being sent in.
-# def average_value(filename, boundary):
-    # hdulist = fits.open(filename)
-    # x_start, x_end = boundary[0][0], boundary[1][0]
-    # y_start, y_end = boundary[0][1], boundary[1][1]
-    # data = hdulist[0].data
-    # region = data[y_start:y_end, x_start:x_end]
-    # avg = np.mean(data)
-    # hdulist.close()
-    # return avg
+def average_value(filename, boundary):
+    hdulist = fits.open(filename)
+    x_start, x_end = boundary[0][0], boundary[1][0]
+    y_start, y_end = boundary[0][1], boundary[1][1]
+    data = hdulist[0].data
+    region = data[y_start:y_end, x_start:x_end]
+    avg = np.mean(region)
+    hdulist.close()
+    return avg,None
 
 # def histogram(boundary):
     # x_start, x_end = boundary[0][0], boundary[1][0]

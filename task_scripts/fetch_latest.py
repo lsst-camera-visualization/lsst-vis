eb719065b177c26a83c5bd597751782d09c307dd
Joe Pagliuco <jbpagliuco@comcast.net>
2016-06-09 14:07:36 -0500

    Update task modify to query the visualization server (checkImage)
    Former-commit-id: 94c504e03cf020bf59deb64324fa6005b8eed95a
    Former-commit-id: a572b318a3b9f0f1f43c7926283cfdb4698f01c3

--- task_scripts/fetch_latest.py
+++ task_scripts/fetch_latest.py
@@ -1,6 +1,13 @@
 
 import task_def
 
+import urllib2
+import json
+
+CHECK_IMAGE_PORT = "8099"
+CHECK_IMAGE_URI = "http://172.17.0.1:" + CHECK_IMAGE_PORT + "/vis/checkImage"
+#CHECK_IMAGE_URI = "http://localhost:" + CHECK_IMAGE_PORT + "/vis/checkImage"
+
 def task(task_params):
 	'''
 	Fetches the latest image from the Rest API (https://github.com/lsst-camera-visualization/visualization-server).
@@ -9,5 +16,10 @@ def task(task_params):
 	@return Returns a dictionary containing the following keys: "uri", "timestamp", "name"
 	'''
 
-	test_dict = { "uri" : "test_uri", "timestamp" : "test_timestamp", "name" : "test_name" };
-	return test_dict, None
+	try:
+		response = urllib2.urlopen(CHECK_IMAGE_URI)
+		raw_data = response.read().decode("utf-8")
+		json_data = json.loads(raw_data)
+		return json_data, None
+	except Exception as e:
+		return { "error" : e }, None

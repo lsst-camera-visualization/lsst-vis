7a874d5c48641a0ac3a59d4d08c59a5a1f3b2f49
Wei Ren <victor-ren@outlook.com>
2016-08-05 14:42:57 -0500

    Changed path in taskDef on CS server
    Former-commit-id: 635d942a4d3304c2bfe3cb13e9a418d9a57b1927
    Former-commit-id: ef36b783195c9e2ba8d9b9552b5a43fbf2e5e197

--- task_scripts/taskDef.py
+++ task_scripts/taskDef.py
@@ -6,8 +6,10 @@ This file contains variable and function definition usable by all tasks.
 ###
 # Hardcoded filenames for tasks to use!
 # TODO: incorporate with a file name management
-IMAGE_ORIGINAL = "/www/static/images/imageE2V.fits"
-IMAGE_DISPLAY = "/www/static/images/imageE2V_trimmed.fits"
+# IMAGE_ORIGINAL = "/www/static/images/imageE2V.fits"
+# IMAGE_DISPLAY = "/www/static/images/imageE2V_trimmed.fits"
+IMAGE_ORIGINAL = "/home/weiren2/lsst/lsst_firefly/frontend/images/imageE2V.fits"
+IMAGE_DISPLAY = "/home/weiren2/lsst/lsst_firefly/frontend/images/imageE2V_trimmed.fits"
 # image_display = "/home/wei/lsst_firefly/frontend/images/image.fits"
 # image_display = "/home/dyue2/apache-tomcat-7.0.63/webapps/static/images/image.fits"
 ###

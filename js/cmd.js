fb82a45133c356c1e676146160be3995c659ab5a
Wei Ren <victor-ren@outlook.com>
2016-08-18 14:22:21 -0500

    Removed ';' that prevented javascript from being loaded.
    Former-commit-id: 28185db51bb8d27d98ebe37def2a4357f4ab5008
    Former-commit-id: 4bdbaf9c34a573966910e4998695c1664f654f43

--- js/cmd.js
+++ js/cmd.js
@@ -199,9 +199,9 @@ cmds = {
 						'There was a problem with executing the average_pixel function',
 						'\n',
 						'Please make sure all parameters were typed in correctly',
-						new BoxText('Data', data, false);
+						new BoxText('Data', data, false)
 					];
-					
+
 					box.setText(boxText);
 				}
 			);

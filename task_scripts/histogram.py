3bd79116968c265e497ec2060e34d5a10d01f319
Wei Ren <victor-ren@outlook.com>
2016-09-09 10:24:49 -0500

    added a simple histogram function
    Former-commit-id: a5400d011e1a0086f8f1b6f086145d155629c604
    Former-commit-id: 4394fe046accd2ad10fd31ea7c15e7b2cfbb47d0

---
+++ task_scripts/histogram.py
@@ -0,0 +1,10 @@
+import numpy as np
+from astropy.io import fits
+
+def task(filename, task_params):
+    try:
+        with fits.open(filename) as fits_object:
+            hdulist = fits_object[0]
+            return np.histogram(hdulist.data, bins='auto')
+    except Exception as e:
+        return ['Error when computing histogram.']

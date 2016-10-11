466a1f0b0026b92250bba3aab46fabee88c3d1b8
Yutong Wang <monicawang_7@yahoo.com>
2016-10-11 15:19:07 -0500

    Histogram finished
    call histogram with file and boundary, creates a json file called data.json that can be used for 'chart' command under frontend forlder
    might need to modify 'chart' command's params, (hard code filename?)
    
    Former-commit-id: bdfde286e9f56d9e160963c99f9452f84d94b501
    Former-commit-id: f05568af611400f71911d95456cc1af5e3eec49a

--- task_scripts/utility_scripts/histogram.py
+++ task_scripts/utility_scripts/histogram.py
@@ -1,20 +1,40 @@
 from astropy.io import fits
 import numpy as np
+import json
+import os.path
 
-def histogram(filename, boundary, bins):
+def _histogram(filename, boundary):
 	hdulist = fits.open(filename)
 	x_start, x_end = boundary[0][0], boundary[0][1]
 	y_start, y_end = boundary[1][0], boundary[1][1]
 	#works for single extension?  what about multi extension? convert coord?
-	file_data = hdulist[?].data
-	print file_data
+	file_data = hdulist[1].data
+	#print file_data
     # data with in the given region
 	region_data = file_data[y_start:y_end, x_start:x_end]
-	hist = np.histogram(region_data, bins)
+	hist = np.histogram(region_data, bins = 'auto')
 	return hist
 
+
+def get_json(hist):
+	labels = hist[1]
+	value = hist[0]
+	values = []
+	for i in range (0, len(hist[1])-1):
+		_bin = {'label' : str(labels[i]) + ' - ' + str(labels[i+1]), 'value' : value[i]}
+		values.append(_bin)
+
+	ret = {"title" : filename, "type" : "Discrete Bar", "data" : {"values" : values}}
+	json.dump(ret, open('../../../frontend/data.json', "w"), indent=4)
+#	return json.dumps(ret, sort_keys = False)
+
+
+def histogram(filename, boundary):
+	get_json(_histogram(filename, boundary))
+
+#Testing
 if __name__ == "__main__":
     filename = "/home/yutong/firefly/backend/images/imageE2V.fits"
     boundary = [[1, 20], [2, 20]]
-    header = histogram(filename, boundary, 5)
+    header = histogram(filename, boundary)
     print header

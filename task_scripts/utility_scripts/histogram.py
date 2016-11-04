95512fc15ef46fec2121686cbc581a9a84351138
Yutong Wang <monicawang_7@yahoo.com>
2016-11-03 23:41:21 -0500

    Add files via upload
    Former-commit-id: c7965846d13f148e9e235784e9a6bfe8e7d94abb
    Former-commit-id: 3092c9ab2b5827f50a423dff83a1f73937d509eb

--- task_scripts/utility_scripts/histogram.py
+++ task_scripts/utility_scripts/histogram.py
@@ -1,46 +1,35 @@
 from astropy.io import fits
+from utility_scripts.helper_functions import parseRegion_rect, circle_mask
+from scipy.ndimage.filters import generic_filter as gf
 import numpy as np
-import json
-import os.path
 
-def _histogram(filename, region, numBins):
-	hdulist = fits.open(filename)
-	x_start, x_end = region[0][0], region[0][1]
-	y_start, y_end = region[1][0], region[1][1]
-	#works for single extension?  what about multi extension? convert coord?
-	file_data = hdulist[1].data
-	#print file_data
-    # data with in the given region
-	region_data = file_data[y_start:y_end, x_start:x_end]
-	hist = np.histogram(region_data, bins = numBins)
+
+def _histogram(fits_object, region_type, value, numBins):
+	file_data = fits_object[1].data
+	if (region_type=='rect'):
+        region_slice = parseRegion_rect(value)
+        ROI = file_data[region_slice]
+        hist = np.histogram(ROI, bins = numBins)
+    elif (region_type=='circ'):
+        mask = circle_mask(file_data, value)
+        hist = gf(file_data, np.histogram, footprint=mask)
+    else
+    	hist = None
 	return hist
 
 
-def get_json(hist):
-	labels = hist[1]
-	value = hist[0]
-	#values = []
-	ret = [[value[0], labels[0], labels[1]]]
-	for i in range (1, len(hist[1])-1):
-		#_bin = {'label' : str(labels[i]) + ' - ' + str(labels[i+1]), 'value' : value[i]}
-		_bin = [[value[i], labels[i], labels[i+1]]]
-		#values.append(_bin)
-		ret = np.concatenate((ret, _bin),axis=0)
-
-	#ret = {"title" : filename, "type" : "Discrete Bar", "data" : {"values" : values}} 
-	#file = open('../../../frontend/data.json', "w")
-	#json.dump(ret, file, indent=4)
-	#file.close()
-	return ret
-
-#	return json.dumps(ret, sort_keys = False)
+def get_data(hist):
+	if hist is None:
+		return None
+	else:
+		labels = hist[1]
+		value = hist[0]
+		ret = [[value[0], labels[0], labels[1]]]
+		for i in range (1, len(hist[1])-1):
+			_bin = [[value[i], labels[i], labels[i+1]]]
+			ret = np.concatenate((ret, _bin),axis=0)
+		return ret
 
 
-def histogram(filename, region, numBins):
-	print get_json(_histogram(filename, region, numBins))
-
-#Testing
-if __name__ == "__main__":
-    filename = "/home/yutong/firefly/backend/images/imageE2V.fits"
-    region = [[1, 5], [1, 5]]
-    histogram(filename, region, 10)
+def histogram(fits_object, region_type, value, numBins):
+	return get_data(_histogram(fits_object, region_type, value, numBins))

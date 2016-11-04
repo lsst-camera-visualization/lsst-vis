58e09ee53cd07b4d9ffc788b14ac22d5bf0ce0d4
Yutong Wang <monicawang_7@yahoo.com>
2016-11-03 22:31:32 -0500

    Add files via upload
    Former-commit-id: 4fa5eefe12644539089e7a152a5f7ca007940654
    Former-commit-id: c8ce90e2e8d5699533776d050c985763d4b34937

--- task_scripts/utility_scripts/histogram.py
+++ task_scripts/utility_scripts/histogram.py
@@ -3,16 +3,16 @@ import numpy as np
 import json
 import os.path
 
-def _histogram(filename, boundary):
+def _histogram(filename, region, numBins):
 	hdulist = fits.open(filename)
-	x_start, x_end = boundary[0][0], boundary[0][1]
-	y_start, y_end = boundary[1][0], boundary[1][1]
+	x_start, x_end = region[0][0], region[0][1]
+	y_start, y_end = region[1][0], region[1][1]
 	#works for single extension?  what about multi extension? convert coord?
-	file_data = hdulist[1].data
+	file_data = hdulist[0].data
 	#print file_data
     # data with in the given region
 	region_data = file_data[y_start:y_end, x_start:x_end]
-	hist = np.histogram(region_data, bins = 'auto')
+	hist = np.histogram(region_data, bins = numBins)
 	return hist
 
 
@@ -25,18 +25,19 @@ def get_json(hist):
 		values.append(_bin)
 
 	ret = {"title" : filename, "type" : "Discrete Bar", "data" : {"values" : values}}
-	file = open('../../../frontend/data.json', "w")
-	json.dump(ret, file, indent=4)
-	file.close()
+	#file = open('../../../frontend/data.json', "w")
+	#json.dump(ret, file, indent=4)
+	#file.close()
+	return ret
 
 #	return json.dumps(ret, sort_keys = False)
 
 
-def histogram(filename, boundary):
-	get_json(_histogram(filename, boundary))
+def histogram(filename, region, numBins):
+	get_json(_histogram(filename, region, numBins))
 
 #Testing
 if __name__ == "__main__":
     filename = "/home/yutong/firefly/backend/images/imageE2V.fits"
-    boundary = [[1, 20], [2, 20]]
-    histogram(filename, boundary)
+    region = [[1, 20], [2, 20]]
+    histogram(filename, region, 10)

5017e7115d9e597c7aa9df50b90ba9f58b078e1c
Yutong Wang <monicawang_7@yahoo.com>
2016-11-07 16:51:44 -0600

    Update chart.py
    Former-commit-id: 8f82cc1ea0dd97dbe9ee231b5f0ed26cef682633
    Former-commit-id: 9b6135fae6796446678b23c64b9fc78e68029d4a

--- task_scripts/chart.py
+++ task_scripts/chart.py
@@ -22,6 +22,6 @@ def task(filename, task_params):
 
 #Testing
 if __name__ == "__main__":
-    filename = "/home/yutong/firefly/backend/images/imageE2V.fits"
+    filename = "http://web.ipac.caltech.edu/staff/roby/demo/wise-m51-band1.fits"
     region = {'x1': 1, 'x2' : 5, 'y1' : 1, 'y2' : 5}
     print task(filename, {'region':{'type': 'rect', 'value' : region}, 'numBins': 10})

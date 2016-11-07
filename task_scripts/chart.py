6d6e223f35d7a9c0f8cc9d53d51a913e47c6bbfa
Yutong Wang <monicawang_7@yahoo.com>
2016-11-07 17:04:00 -0600

    Update chart.py
    Former-commit-id: e93c567e65160aca77c46a951df63b83eefe1c9a
    Former-commit-id: 28edae726e9b29cca3d91f496efff39e6f8cdcf8

--- task_scripts/chart.py
+++ task_scripts/chart.py
@@ -24,4 +24,4 @@ def task(filename, task_params):
 if __name__ == "__main__":
     filename = "http://web.ipac.caltech.edu/staff/roby/demo/wise-m51-band1.fits"
     region = {'x1': 1, 'x2' : 5, 'y1' : 1, 'y2' : 5}
-    print task(filename, {'region':{'type': 'rect', 'value' : region}, 'numBins': 10})
+    #print task(filename, {'region':{'type': 'rect', 'value' : region}, 'numBins': 10})

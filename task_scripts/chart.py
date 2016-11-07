efd63a20ca1aa45ad187e4cb3f9741bf7aeb23f3
Yutong Wang <monicawang_7@yahoo.com>
2016-11-07 15:47:59 -0600

    backend function for general histogram
    Former-commit-id: 282cdf29e63295f86111eda401c4d53601593999
    Former-commit-id: 600c49538d4507259ec5756c9580054c6a64dc97

--- task_scripts/chart.py
+++ task_scripts/chart.py
@@ -2,19 +2,20 @@ from astropy.io import fits
 from utility_scripts.histogram import histogram
 
 def task(filename, task_params):
-    try:
-        with fits.open(filename) as fits_object:
-        	region_type, value, numBins = task_params['type'], task_params['value'], task_params['numBins']
-        	ret =  histogram(fits_object, region_type, value, numBins)
-        	if ret is None:
-        		ret = "Region type is not recognized."
-        	return {"data":ret}, None
-    except Exception as e:
-        return ['Error when computing histogram.']
+	try:
+	    with fits.open(filename) as fits_object:
+	    	region_type, value, numBins = task_params['type'], task_params['value'], task_params['numBins']
+	    	ret =  histogram(fits_object, region_type, value, numBins)
+	    	if ret is None:
+	    		ret = "Region type is not recognized."
+	    	return {"data":ret}, None
+	except Exception as e:
+		return ['Error when computing histogram.']
+
 
 
 #Testing
 if __name__ == "__main__":
     filename = "/home/yutong/firefly/backend/images/imageE2V.fits"
-    region = [[1, 5], [1, 5]]
-    print task(filename, {'type': 'rect', 'value' = region, 'numBins': 10})
+    region = {'x1': 1, 'x2' : 5, 'y1' : 1, 'y2' : 5}
+    print task(filename, {'type': 'rect', 'value' : region, 'numBins': 10})

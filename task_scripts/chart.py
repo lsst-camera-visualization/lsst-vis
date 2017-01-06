744e021fe1404d25e7605ed3ca34311b060d1797
Yutong Wang <monicawang_7@yahoo.com>
2017-01-05 21:34:18 -0600

    histogram update
    now return an array start with underflow bins, end with overflow bins
    
    Former-commit-id: a14a41c99d3325657ff621736df29fd5136d8625
    Former-commit-id: 5449fa33517b8281e930d3fab33d23a57399a0ab

--- task_scripts/chart.py
+++ task_scripts/chart.py
@@ -9,18 +9,15 @@ def task(filename, task_params):
 	@return data for general histogram function
 	'''
 
-	try:
-		with fits.open(filename) as fits_object:
-			region = task_params['region']
-			region_type, value, numBins = region['type'], region['value'], task_params['numBins']
-			ret =  histogram(fits_object, region_type, value, numBins)
-			if ret is None:
-				ret = "Region type is not recognized."
-			return {"data":ret}, None
-	except Exception as e:
-		return ["Error when computing histogram."], None
+	with fits.open(filename) as fits_object:
+		region = task_params['region']
+		region_type, value, numBins, _min, _max = region['type'], region['value'], task_params['numBins'], task_params['min'], task_params['max']
+		ret =  histogram(fits_object, region_type, value, numBins, _min, _max)
+		if ret is None:
+			ret = "Region type is not recognized."
+		return {"data":ret.tolist()}, None
 # Testing
 if __name__ == "__main__":
 	filename = "http://web.ipac.caltech.edu/staff/roby/demo/wise-m51-band1.fits"
 	region = {'x1': 100, 'x2' : 200, 'y1' : 100, 'y2' : 200}
-	print(task(filename, {'region':{'type': 'rect', 'value' : region}, 'numBins': 10}))
+	print(task(filename, {'region':{'type': 'rect', 'value' : region}, 'numBins': 10, 'min':4, 'max':20}))
--- task_scripts/utility_scripts/histogram.py
+++ task_scripts/utility_scripts/histogram.py
@@ -4,32 +4,56 @@ from scipy.ndimage.filters import generic_filter as gf
 import numpy as np
 
 
-def _histogram(fits_object, region_type, value, numBins):
+underflow_bins = 0
+overflow_bins = 0
+min_value = 0
+max_value = 0
+def _histogram(fits_object, region_type, value, numBins, _min, _max):
+	global underflow_bins
+	global overflow_bins
+	global min_value
+	global max_value
 	file_data = fits_object[0].data
 	if (region_type=='rect'):
 		region_slice = parseRegion_rect(value)
 		ROI = file_data[region_slice]
-		hist = np.histogram(ROI, bins = numBins)
+		underflow_bins = (ROI < _min).sum()
+		overflow_bins = (ROI > _max).sum()
+		min_value = np.amin(ROI)
+		max_value = np.amax(ROI)
+		print(max_value)
+		hist = np.histogram(ROI, bins = numBins, range=(_min, _max))
 	elif (region_type=='circ'):
 		mask = circle_mask(file_data, value)
+		underflow_bins = (mask < _min).sum()
+		overflow_bins = (mask > _max).sum()
+		min_value = np.amin(mask)
+		max_value = np.amax(mask)
 		hist = gf(file_data, np.histogram, footprint=mask)
 	else:
 		hist = None
 	return hist
 
 
-def get_data(hist):
+def get_data(hist,_min, _max):
 	if hist is None:
 		return None
 	else:
 		labels = hist[1]
 		value = hist[0]
-		ret = [[value[0], labels[0], labels[1]]]
-		for i in range (1, len(hist[1])-1):
+		ret = [[underflow_bins, min_value, _min]]
+		_bin = [[0,_min, _min]]
+		ret = np.concatenate((ret, _bin),axis=0)
+		for i in range (0, len(hist[1])-1):
 			_bin = [[value[i], labels[i], labels[i+1]]]
 			ret = np.concatenate((ret, _bin),axis=0)
-		return ret.tolist()
+		_bin = [[0, _max, _max]]
+		ret = np.concatenate((ret, _bin),axis=0)
+		_bin = [[overflow_bins, _max, max_value]]
+		ret = np.concatenate((ret, _bin),axis=0)
+		return ret
 
 
-def histogram(fits_object, region_type, value, numBins):
-	return get_data(_histogram(fits_object, region_type, value, numBins))
+def histogram(fits_object, region_type, value, numBins, _min, _max):
+	ret = get_data(_histogram(fits_object, region_type, value, numBins, _min, _max),_min, _max)
+	return ret

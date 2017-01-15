bfed27452b2758fa3981bcbb69b4b85272caaa6d
Wei Ren <victor-ren@outlook.com>
2017-01-15 11:15:58 +0800

    fixed boundary for overscan regions
    Former-commit-id: 9d9066dc9cc7e51dd865fee3bcfc7155c0ee25b5
    Former-commit-id: e74075f6c1b8e58c0560273ca516a8836d059c4b

--- js/utility.js
+++ js/utility.js
@@ -3,9 +3,12 @@ function read_boundary(data, cb, viewer) {
         function(data) {
         console.log(data);
         var d = data.BOUNDARY;
+        if (viewer.overscan){
+            d = data.BOUNDARY_OVERSCAN;
+        }
         var regions = [];
         var color = 'red';
-        for (var i = 0; i < d.length; i++) { //TODO: error handling!
+        for (var i = 0; i < d.length; i++) {
             var di = d[i];
             for (var j = 0; j < di.length; j++) {
                 var dij = di[j];
@@ -15,9 +18,10 @@ function read_boundary(data, cb, viewer) {
                 var y = dij['y'];
                 var content = ['box', x, y, width, height, 0, '#color=' + color].join(' ');
                 regions.push(content);
-              }
+            }
         }
-            cb({"header":data, "regions_ds9":regions});
+        console.log(regions);
+        cb({"header":data, "regions_ds9":regions});
         },
         function(data) {
             LSST.state.term.lsst_term('echo', 'There was a problem when fetching boundary information of FITS file.');

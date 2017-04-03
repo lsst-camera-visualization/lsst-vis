999add108c6434a2a3ff4a75e91f4f4b74ac4ecb
Joe Pagliuco <jbpagliuco@comcast.net>
2017-04-03 13:42:16 -0500

    Added firefly region interface
    Former-commit-id: 6e177b6eb6d6e8378fb28395e10fc29c02aa3a57
    Former-commit-id: adddaccd2fa5d2e24cea5e031f604a93f5c56137

--- src/js/util/FireflyUtil.js
+++ src/js/util/FireflyUtil.js
@@ -1,6 +1,29 @@
-// Firefly interface
+import { Util } from "./Util";
 
+// Firefly interface
 export const FireflyUtil = {
+    // Clears a region layer on a viewer
+    ClearRegion: (plotID, layer) => {
+        firefly.action.dispatchDeleteRegionLayer(layer, plotID);
+    },
+
+    // Draws DS9 regions on a viewer
+    DrawRegions: (plotID, layer, regions, options) => {
+        // Default values for options
+        options = Object.assign({
+            color: "white",
+            width: 1
+        }, options);
+
+        // Creates the DS9 regions
+        const regionsToDraw = regions.map( r =>
+            "image; " + r + " # color=" + options.color + " width=" + options.width + ";"
+        );
+
+        firefly.action.dispatchCreateRegionLayer(layer, layer, null, regionsToDraw, plotID);
+    },
+
+    // Loads an image into the plot id
     LoadImage: (plotID, imageURL) => {
         firefly.showImage(plotID, {
             plotId: plotID,
@@ -11,6 +34,7 @@ export const FireflyUtil = {
         });
     },
 
+    // Launches a backend task
     LaunchTask: (taskName, params, viewer) => {
         params.image_url = (taskName === "boundary") ? viewer.original_image_url : viewer.image;
 

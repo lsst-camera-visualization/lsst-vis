553aef61004d978c9666298104dcdac50d038005
Joe Pagliuco <jbpagliuco@comcast.net>
2017-04-05 14:04:34 -0500

    hovered amp name

--- src/js/util/viewer.js
+++ src/js/util/viewer.js
@@ -1,5 +1,5 @@
 import { AddActionListener } from "./firefly";
-import { drawDS9Regions, updateCursorPos } from "../actions/viewer.actions";
+import { drawDS9Regions, updateCursorPos, updateHoveredAmpName } from "../actions/viewer.actions";
 import loadBoundary from "../commands/loadBoundary";
 
 import store from "../store";
@@ -29,9 +29,67 @@ export class Viewer {
         this.image = image;
     }
 
+    calculateHoveredSeg = () => {
+        if (this.header) {
+            const boundary = this.header.BOUNDARY_OVERSCAN || this.header.BOUNDARY;
+            const numAmps = this.header.NUM_AMPS;
+            const segSize = this.header.SEG_SIZE || this.header.SEG_DATASIZE;
+
+            const h = {
+                x: Math.floor(this.cursorPoint.x / segSize.x),
+                y: numAmps.y - 1 - Math.floor(this.cursorPoint.y / segSize.y)
+            };
+            if (h.y >= 0 && h.x >= 0 && h.y < boundary.length && h.x < boundary[0].length)
+                return h;
+        }
+
+        return null;
+    }
+
+    calculateHoveredAmpName = () => {
+        const boundary = this.header.BOUNDARY_OVERSCAN || this.header.BOUNDARY;
+        const segSize = this.header.SEG_SIZE || this.header.SEG_DATASIZE;
+        const overscan = {
+            pre: this.header.OVERSCAN.PRE,
+            post: this.header.OVERSCAN.POST,
+            over: this.header.OVERSCAN.OVER
+        };
+
+        const h = this.calculateHoveredSeg();
+        let name = null;
+        if (h) {
+            name = "amp" + h.y.toString() + h.x.toString();
+            let s = {
+                x: this.cursorPoint.x % segSize.x,
+                y: this.cursorPoint.y % segSize.y
+            };
+
+            const seg = boundary[h.y][h.x];
+            if (seg.reverse_slice.y)
+                s.y = segSize.y - s.y;
+            if (seg.reverse_slice.x)
+                s.x = segSize.x - s.x;
+
+            if (s.y >= overscan.over)
+                name += "overscan";
+            else if (s.x < overscan.pre)
+                name += "prescan";
+            else if (s.x >= overscan.post)
+                name += "postscan";
+            else
+                name += "data";
+        }
+
+        return name;
+    }
+
     onCursorMove = action => {
         const imgPt = action.payload.readoutItems.imagePt;
         if (imgPt)
             store.dispatch(updateCursorPos(this.id, imgPt.value));
+
+        const name = this.calculateHoveredAmpName();
+        if (name)
+            store.dispatch(updateHoveredAmpName(this.id, name));
     }
 }

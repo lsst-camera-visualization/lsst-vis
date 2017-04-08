707e68c1608db5f84fe17ae4c4bd5ca47acc5cf1
Joe Pagliuco <jbpagliuco@comcast.net>
2017-04-07 20:39:40 -0500

    graph pixels
    Former-commit-id: 1361b4ede5cc536766a8e17221af75284cff677c
    Former-commit-id: 108a96b746a66f6e913ceef67468d7b66923395d

--- src/task_scripts/graph_pixel.py
+++ src/task_scripts/graph_pixel.py
@@ -1,25 +1,20 @@
 import os
-from astropy.io import fits
-from utility_scripts.histogram import histogram
 
-def task(filename, task_params):
-	'''
-	@author Yutong Wang
-	@param task_params - {type : rect/circ, value : region, numBins : int, min/double : int, max: int/double}
-	@return data for general histogram function
-	'''
-	try:
-		with fits.open(filename) as fits_object:
-			region = task_params['region']
-			region_type, value, numBins, _min, _max = region['type'], region['value'], task_params['numBins'], task_params['min'], task_params['max']
-			ret =  histogram(fits_object, region_type, value, numBins, _min, _max)
-			if ret is None:
-				ret = "Region type is not recognized."
-			return {"data":ret.tolist()}, None
-	except Exception as e:
-		return ["Error when computing histogram."], e
-# Testing
-if __name__ == "__main__":
-	filename = "http://web.ipac.caltech.edu/staff/roby/demo/wise-m51-band1.fits"
-	region = {'x1': 100, 'x2' : 200, 'y1' : 100, 'y2' : 200}
-	print(task(filename, {'region':{'type': 'rect', 'value' : region}, 'numBins': 10, 'min':2, 'max':4}))
+def task(filename, taskParams):
+    '''
+    @author Joe Pagliuco
+    @param taskParams
+    @return data for general histogram function
+    '''
+
+    with Image(filename) as img:
+        # Create the region object
+        regionType, regionValue = taskParams["region"]["type"], taskParams["region"]["value"]
+        region = Region(regionType, regionValue)
+        numBins, minValue, maxValue = taskParams["bins"], taskParams["min"], taskParams["max"]
+
+        def histogram(a):
+            return np.histogram(a, bins=numBins, (minValue, maxValue))
+        h = region.execute(img, histogram)
+
+    return { "result":  }, None
--- src/task_scripts/noise.py
+++ src/task_scripts/noise.py
@@ -2,7 +2,7 @@ import numpy as np
 from util.image import Image
 from util.region import Region
 
-def task(filename, task_params):
+def task(filename, taskParams):
     ''' Calculates the noise over the region.
     @author Joe Pagliuco
     @param filename - file name of the FITS image
@@ -19,3 +19,8 @@ def task(filename, task_params):
         noise = region.execute(img, secondMoment)
 
     return { "result": str(noise) }, None
+
+if __name__ == "__main__":
+	filename = "http://web.ipac.caltech.edu/staff/roby/demo/wise-m51-band1.fits"
+	region = {'x1': 100, 'x2' : 200, 'y1' : 100, 'y2' : 200}
+	print(task(filename, {'region':{'type': 'rect', 'value' : region}}))

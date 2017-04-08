25d930fbd3a165cc3046ef2efdf2302f76ad54c1
Joe Pagliuco <jbpagliuco@comcast.net>
2017-04-07 21:51:16 -0500

    graph pixel, seperating underflow, overflow, and main hist
    Former-commit-id: 83fb05e2b0e5b4ee0552045094b1d2087b2130b0
    Former-commit-id: 0f6bc10f92e0dab60df9abf8c8e8cd77d467643b

---
+++ src/task_scripts/graphPixel.py
@@ -0,0 +1,37 @@
+import numpy as np
+from util.image import Image
+from util.region import Region
+from util.histogram import NumpyToFireflyHist
+
+def task(filename, taskParams):
+    '''
+    @author Joe Pagliuco
+    @param taskParams
+    @return data for general histogram function
+    '''
+
+    with Image(filename) as img:
+        # Create the region object
+        regionType, regionValue = taskParams["region"]["type"], taskParams["region"]["value"]
+        region = Region(regionType, regionValue)
+        numBins, minValue, maxValue = taskParams["bins"], taskParams["min"], taskParams["max"]
+
+        # Largest floating point number
+        maxFloat = np.finfo(float).max
+        def histogram(a):
+            return np.histogram(a, bins=numBins, range=(minValue, maxValue))
+        def underflow(a):
+            return np.histogram(a, bins=1, range=(-maxFloat, minValue - np.finfo(float).eps))
+        def overflow(a):
+            return np.histogram(a, bins=1, range=(maxValue + np.finfo(float).eps, maxFloat))
+
+        # Gives us the histogram for the values in the range
+        h = region.execute(img, histogram)
+        u = region.execute(img, underflow)
+        o = region.execute(img, overflow)
+
+        fireflyHist = NumpyToFireflyHist(h)
+        underflow = NumpyToFireflyHist(u)
+        overflow = NumpyToFireflyHist(o)
+
+    return { "data": fireflyHist, "underflow": underflow, "overflow": overflow }, None

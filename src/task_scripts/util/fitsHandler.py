57adc0ad9cb5baa9b4ebdbf08b16dda7ea2a606d
Wei <victor-ren@outlook.com>
2017-04-10 00:55:52 -0500

    added getImageData function
    Former-commit-id: c60713f6fb76ae0f1b08e465ae433a618820f254
    Former-commit-id: 48d556ea8c3d6de02f6d9d60c5f5cf655718af16

--- src/task_scripts/util/fitsHandler.py
+++ src/task_scripts/util/fitsHandler.py
@@ -1,43 +1,112 @@
 from astropy.io import fits
+import re
 
 class fitsHandler(object):
     '''A class for dealing with FITS images in python. It provides
     wrapper functions to get FITS header information and image data.
+
+    Always use `with` statement declaring the object.
+
+    Note: currently CCD level FITS images are mostly used. Here's an
+    example of the data layout in general.
+        Segment/amplifier coordinates in a CCD.
+        Format: amplifier[Y][X] (origin at top left corner).
+        +----+----+----+----+----+----+----+----+
+        | 00 | 01 | 02 | 03 | 04 | 05 | 06 | 07 |
+        +----+----+----+----+----+----+----+----+
+        | 10 | 11 | 12 | 13 | 14 | 15 | 16 | 17 |
+        +----+----+----+----+----+----+----+----+
+
+    HDU stands for Header/Data Units (HDUs).
     '''
 
     def __init__(self, imageName):
         self.imageName = imageName;
-        self.image = None
+        self.hduList = None
+        self.imageHDUs = None
+        self.isMulti = None
+        self.primaryHDU = None
 
     def __enter__(self):
         try:
-            self.image = fits.open(self.imageName)
+            self.hduList = fits.open(self.imageName)
+            self.primaryHDU = (self.hduList)[0]
+            primaryHeader = (self.primaryHDU).header
+            self.isMulti = ("EXTEND" in primaryHeader) and primaryHeader["EXTEND"]
+            if (self.isMulti):
+                self.imageHDUs = [elem in self.hduList if type(elem).__name__ == 'ImageHDU']
+            else:
+                self.imageHDUs = (self.hduList)[0] # primary header
         except Exception as e:
             print("Cannot open the FITS file.")
             raise
 
     def __exit__(self):
         try:
-            self.image.close()
+            self.hduList.close()
         except AttributeError as e: # Ignore if there is no image
             pass
         except Exception as e:
             raise
 
-    def getHeader(self, withOverscan=False):
-        '''Retrive the header information from FITS file.
+    def __getImageHDUFromHDUList(self, segPositio=""):
+        ''' Helper function. Iterate through HDU list and return one particular
+        image HDU.
+        @author Wei Ren
+        @param segPosition - see getImageData
+        @return A pointer to imageHDU object. Return None if none found.
+        '''
+        for elem in self.imageHDUs:
+            if (elem.header)["EXTNAME"] == "Segment" + segPosition:
+                return elem
+        return None
+
+    def getHeader(self):
+        '''Retrive the header information from FITS file. The information is
+        stored as python dictionary.
         @author Wei Ren
         @param None
         @return A dictionary of FITS header keyword and values.
         '''
         pass
 
-    def getImageData(self, extensionPosition=""):
-        '''Retrive the image data as a two dimensional array.
+    def getHeaderJSON(self):
+        '''Retrive the header information in JSON format. It is useful for
+        the communication of frontend and backend.
         @author Wei Ren
-        @param index (optional) - speicify the "position" of an extension if
-                                it's multi-extension FITS. On single-extension
-                                FITS this argument is ignored.
-        @return A 2-D numpy array of pixel values.
+        @param
+        @return JSON object of header information (including segments)
         '''
         pass
+
+    def getImageData(self, segPosition=""):
+        '''Retrive the image data as a two dimensional array. It retrives data
+        of one segment if the FITS is in multi-extension format.
+        @author Wei Ren
+        @param segPosition - String (optional) speicify the "position" of
+                                an extension if it is a multi-extension FITS.On
+                                single-extension FITS this argument is ignored.
+        @return A 2-D numpy array of pixel values.
+        '''
+        if self.isMulti:
+            if not re.match(r"^\d{2}$", segPosition):
+                raise ValueError("Wrong argument segPosition passed in. Expected to be string of two digits, e.g. "17")
+            hdu = self.__getImageHDUFromHDUList(segPosition)
+            return hdu.data
+        else:
+            return self.primaryHDU.data
+
+    def getSegmentHeader(self, segPosition=""):
+        '''Retrive the header of one segment. It will return the primary header
+        if the FITS image is single-extension.
+        @author Wei Ren
+        @param segPosition - see getImageData
+        @return A pointer to the astropy.fits header object.
+        '''
+        if self.isMulti:
+            if not re.match(r"^\d{2}$", segPosition):
+                raise ValueError("Wrong argument segPosition passed in. Expected to be string of two digits, e.g. "17")
+            hdu = self.__getImageHDUFromHDUList(segPosition)
+            return hdu.header
+        else:
+            return self.primaryHDU.header

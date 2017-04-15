46a022dae6d03fb84a94eb8e4fbd5c9253631230
Joe Pagliuco <jbpagliuco@comcast.net>
2017-04-14 20:20:58 -0500

    bad command input bug fix
    Former-commit-id: 3a7fd5d4fe5735e8be879b7bb18c1255e7a3124a
    Former-commit-id: 36d27a686c610847ed933c30e13e4553efa3979a

--- src/js/reducers/terminal.reducer.js
+++ src/js/reducers/terminal.reducer.js
@@ -1,7 +1,11 @@
 import { Terminal } from "../util/terminal";
+import { JSUtil } from "../util/jsutil";
 
-const lastState = JSON.parse(localStorage.getItem("state")).terminal;
-const initialState = new Terminal().loadFromState(lastState);
+const lastState = JSON.parse(localStorage.getItem("state"));
+const initialState = new Terminal();
+if (lastState && !JSUtil.IsEmptyObject(lastState.terminal))
+    initialState.loadFromState(lastState.terminal);
+initialState.defaults = { viewer_id: "ffview", box_id: "ffbox" }
 
 const terminalReducer = (state = initialState, action) => {
     switch (action.type) {

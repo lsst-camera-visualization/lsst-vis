4ccf68d64cb10446bdbaaa8da975bafb75a13aa6
Joe Pagliuco <jbpagliuco@comcast.net>
2017-04-16 15:12:18 -0500

    Error messages in terminal
    Former-commit-id: 040c5f4e8a9958d70e3597d15b4e2b35c67d52f8
    Former-commit-id: 862fb53150b08b47e346a767bc46408c005d8471

--- src/js/components/Terminal/TerminalBody.js
+++ src/js/components/Terminal/TerminalBody.js
@@ -1,16 +1,23 @@
 import React from "react";
 
 export default class TerminalBody extends React.Component {
+    createEntry = (entry, i) => {
+        switch (entry.type) {
+            case "COMMAND": {
+                return <p className="term-body-entry" key={i}>{"~> " + entry.msg}</p>
+            }
+
+            case "ERROR": {
+                return <p className="term-body-error" key={i}>{"Error: " + entry.msg}</p>
+            }
+
+            default:
+                return null;
+        }
+    }
+
     createHistory = () => {
-        return (
-            <ul>
-                { this.props.terminal.history.map(
-                    (entry, i) =>
-                        <li className="term-body-entry" key={i} >{"~> " + entry}</li>
-                    )
-                }
-            </ul>
-        );
+        return <ul>{this.props.terminal.history.map(this.createEntry)}</ul>
     }
 
     componentDidUpdate() {

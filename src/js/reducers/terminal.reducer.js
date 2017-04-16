a77267da0d9e45a9f3823b3350bbf41e051acbec
Joe Pagliuco <jbpagliuco@comcast.net>
2017-04-16 12:16:58 -0500

    moving terminal state into terminal
    Former-commit-id: d20f6e939d3a2cea45235e269454ee33e16b4601
    Former-commit-id: 9bf0d87e5c8aebed6e171d87e745361dc490c875

--- src/js/reducers/terminal.reducer.js
+++ src/js/reducers/terminal.reducer.js
@@ -7,16 +7,37 @@ if (lastState && !JSUtil.IsEmptyObject(lastState.terminal))
     initialState.loadFromState(lastState.terminal);
 initialState.defaults = { viewer_id: "ffview", box_id: "ffbox" }
 
-const terminalReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case "ADD_COMMAND_TO_HISTORY":
-            let newState = state.clone();
-            newState.addEntry(action.payload.plainInput);
-            return newState;
 
-        default:
-            return state;
-    }
+const commands = {
+    "ADD_COMMAND": (state, action) => {
+        let newState = state.clone();
+        newState.addCommand(action.commandName);
+        return newState;
+    },
+
+    "ADD_COMMAND_TO_HISTORY": (state, action) => {
+        let newState = state.clone();
+        newState.addEntry(action.payload.plainInput);
+        return newState;
+    },
+
+    "ADD_PARAMETER_DESC": (state, action) => {
+        let newState = state.clone();
+        newState.setParameterDesc(action.parameter, action.desc);
+        return newState;
+    },
+
+    "SET_DEFAULT": (state, action) => {
+        let newState = state.clone();
+        newState.setDefault(action.parameter_id, action.value);
+        return newState;
+    },
+}
+
+const terminalReducer = (state = initialState, action) => {
+    if (action.type in commands)
+        return commands[action.type](state, action);
+    return state;
 }
 
 export default terminalReducer;

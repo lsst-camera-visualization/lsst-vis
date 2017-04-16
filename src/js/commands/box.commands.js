6c614cd4942897efacc96634a8de0bc5c2a9e138
Joe Pagliuco <jbpagliuco@comcast.net>
2017-04-16 15:58:38 -0500

    Default parameters adjust when we create/delete the first/last ui element
    Former-commit-id: 7fab9af72476f25e9d0d56f4e6df1ac97b6fa1f3

--- src/js/commands/box.commands.js
+++ src/js/commands/box.commands.js
@@ -1,13 +1,23 @@
 import * as BoxActions from "../actions/box.actions";
+import { setDefault } from "../actions/terminal.actions";
+import { JSUtil } from "../util/jsutil";
 
 import store from "../store";
 
 export const createBox = params => {
     store.dispatch(BoxActions.createBox(params.box_id));
+
+    // If we are the only box, we automatically become the default
+    if (JSUtil.ObjectToArray(store.getState().boxes).length === 1)
+        store.dispatch(setDefault("box_id", params.box_id));
 }
 
 export const deleteBox = params => {
     store.dispatch(BoxActions.deleteBox(params.box_id));
+
+    // If we were the last viewer, reset the default
+    if (JSUtil.ObjectToArray(store.getState().boxes).length === 0)
+        store.dispatch(setDefault("box_id", ""));
 }
 
 export const clearBoxText = params => {

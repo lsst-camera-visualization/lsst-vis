648590135cb95aeceeff04ace1be25d6436feb7d
Joe Pagliuco <jbpagliuco@comcast.net>
2017-05-01 13:09:02 -0500

    pause resume button, clear old interval on setInterval
    Former-commit-id: 50a675dca1668a77377d73ef7fa958ad7dfceb47

--- src/js/components/Viewer/ViewerUVPanel.js
+++ src/js/components/Viewer/ViewerUVPanel.js
@@ -1,11 +1,35 @@
 import React from "react";
 
 export default class ViewerUVPanel extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            "paused": true
+        }
+    }
+
+    // Pause/Resume button
+    handlePRClick = e => {
+        if (e.target.value === "Pause") {
+            this.props.onPause();
+            this.setState({paused: true});
+        }
+        else { // Resume
+            this.props.onResume();
+            this.setState({paused: false});
+        }
+    }
+
     render() {
         return (
             <div className="viewer-info-panel viewer-uvPanel">
                 <p className="viewer-info-header">Update Viewer Settings</p>
-                <input className="button" type="button" value="Resume"/>
+                <input
+                    className="button"
+                    type="button"
+                    value={this.state.paused ? "Resume" : "Pause"}
+                    onClick={this.handlePRClick} />
                 <input className="button" type="button" value="There are no new images"/>
             </div>
         );

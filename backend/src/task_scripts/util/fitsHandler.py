17f7172f6880edd6c81651b4dad4aaae23301abd
Wei <victor-ren@outlook.com>
2017-07-07 13:57:02 -0700

    comply with the new header format

--- backend/src/task_scripts/util/fitsHandler.py
+++ backend/src/task_scripts/util/fitsHandler.py
@@ -1,8 +1,9 @@
 from astropy.io import fits
 import re
+import copy
 
 def _convertHeaderRange(detsizeString):
-    '''Convert the boundary coordincates from string(as in the header) to values. It's assumed to be a rectangular region (2-dimensional).
+    '''Convert the boundary coordinates from string(as in the header) to values. It's assumed to be a rectangular region (2-dimensional).
 
     NOTE: slicing are inclusive in the header value.
 
@@ -21,10 +22,11 @@ class fitsHandler:
     '''A class for dealing with FITS images in python. It provides
     wrapper functions to get FITS header information and image data.
 
-    Always use `with` statement declaring the object.
+    Note 1: Always use `with` statement when declaring the object.
 
-    Note: currently CCD level FITS images are mostly used. Here's an
-    example of the data layout in general.
+    Note 2: Only works with single-extension FITS file.
+
+    Note 3: Here's an example of the data layout in general.
         Segment/amplifier coordinates in a CCD.
         Format: amplifier[Y][X] (origin at top left corner).
         +----+----+----+----+----+----+----+----+
@@ -34,27 +36,27 @@ class fitsHandler:
         +----+----+----+----+----+----+----+----+
 
     HDU stands for Header/Data Units (HDUs).
+    @author - Wei Ren
     '''
 
     def __init__(self, imageName):
         self.imageName = imageName
         self.hduList = None
-        self.imageHDUs = None
-        self.isMulti = None
         self.primaryHDU = None
+        self.header = None
+        self.overscan = False
+        self.level = None
+        self.__enter__()
 
     def __enter__(self):
         try:
             self.hduList = fits.open(self.imageName)
             self.primaryHDU = (self.hduList)[0]
-            primaryHeader = (self.primaryHDU).header
-            self.isMulti = ("EXTEND" in primaryHeader) and primaryHeader["EXTEND"]
-            if (self.isMulti):
-                self.imageHDUs = [elem for elem in self.hduList if type(elem).__name__ == 'ImageHDU']
-            else:
-                self.imageHDUs = (self.hduList)[0] # primary header
+            self.header = self.primaryHDU.header
+            self.overscan = self.header["OVERSCAN"]
+            self.level = self.header["LEVEL"]
         except Exception as e:
-            print("Cannot open the FITS file.")
+            print("Cannot open the FITS file or the header lacks necessary information.")
             raise
         return self
 
@@ -66,24 +68,102 @@ class fitsHandler:
         except Exception as e:
             raise
 
-    def __getImageHDUFromHDUList(self, segPosition=""):
-        ''' Helper function. Iterate through HDU list and return one particular
-        image HDU.
-        @author Wei Ren
-        @param segPosition - see getImageData
-        @return A pointer to imageHDU object. Return None if none found.
+    def close(self):
+        self.hduList.close()
+
+    def getHeader(self):
+        ''' Return the header of the (single-extension) FITS file as an
+        header object.
+        @param None
+        @return A HDU header object
         '''
-        for elem in self.imageHDUs:
-            if (elem.header)["EXTNAME"] == "Segment" + segPosition:
-                return elem
-        return None
+        return self.header
+
+    def getHeaderJSON(self):
+        '''Retrive the header information in JSON compatible format
+        (python dictionary). It is used in the communication of frontend
+        and backend.
+        @param - None
+        @return - Python dictionary of header informations
+        '''
+        if self.level == "CCD":
+            return self.getCCDHeaderJSON()
+        elif self.level == "RAFT":
+            return self.getRaftHeaderJSON()
+        else:
+            raise NameError("Unknown level for this FITS: " + self.level)
+
+    def getCCDHeaderJSON(self, R_YX="R99", S_YX="S99"):
+        '''Retrive the header information on a CCD-level FITS.
+        @param - None
+        @return - Python dictionary of header informations
+        '''
+        prefix = "HIERARCH {} {} SEGMENT".format(R_YX, S_YX)
+        tempSeg = "00"
+        header = self.header
+        segDim = {"x": header[prefix + tempSeg + " NAXIS1"],
+                    "y": header[prefix + tempSeg + NAXIS2]}
+
+    def getRaftHeaderJSON(self):
+        '''Retrive the header information on a raft-level FITS.
+        @param - None
+        @return - Python dictionary of header informations
+        '''
+        pass
+
+    def __rangeToDim(self, region):
+        ''' Private helper function for converting a range/slice into
+        a dimenstion
+        @param: region - region in 2D (dictionary with keys: x1, x2, y1, y2)
+        @return: dimension - dictionary with keys: x, y
+        '''
+        return {"x": 1 + abs(region["x2"]-region["x1"]),
+                "y": 1 + abs(region["y2"]-region["y1"])}
+
+    def __addOffset(self, region, xOffset, yOffset):
+        ''' Private helper function to add x and y offset to a region.
+        @param: region - region in 2D (dictionary with keys: x1, x2, y1, y2)
+        @param: xOffset - offset in x to add
+        @param: yOffset - offset in y to add
+        @return: the new range (with offset added)
+        '''
+        return {"x1": region["x1"] + xOffset,
+                "x2": region["x2"] + xOffset,
+                "y1": region["y1"] + yOffset,
+                "y2": region["y2"] + yOffset}
+
+    def __rangeComplement(self, region, segDim, x=False, y=False):
+        ''' Private helper function to convert the region if the
+        coordinates are reversed in the header.
+        @param: region - region in 2D (dictionary with keys: x1, x2, y1, y2)
+        @param: segDim - Dimension of the segment/amplifier
+        @param: x - reversed in x direction or not
+        @param: y - reversed in y direction or not
+        @return: new region with the correct orientation
+        '''
+        newRegion = copy.deepcopy(region)
+        if x:
+            newRegion["x1"] -= (segDim["x"] - 1)
+            newRegion["x2"] -= (segDim["x"] - 1)
+        if y:
+            newRegion["y1"] -= (segDim["y"] - 1)
+            newRegion["y2"] -= (segDim["y"] - 1)
+        return newRegion
+
+
+    def __formatRect(self, region):
+        ''' Private helper function to convert a region into Rect (used in frontend).
+        @param: region - range in 2D (dictionary with keys: x1, x2, y1, y2)
+        @return: Rect
+        '''
+        return {"type": "rect", "data": region}
 
     def getHeader(self):
         '''Retrive the header information from FITS file. The information is
         stored as python dictionary.
         @author Wei Ren
-        @param None
-        @return A dictionary of FITS header keyword and values.
+        @param - None
+        @return - A dictionary of FITS header keyword and values.
         '''
         def _convertRangeToDim(region):
             return {"x": 1 + abs(region["x2"]-region["x1"]),
@@ -163,43 +243,9 @@ class fitsHandler:
         return {"type": "CCD-OVERSCAN",
                 "data": boundaryArray}
 
-    def getHeaderJSON(self):
-        '''Retrive the header information in JSON format. It is used in
-        the communication of frontend and backend.
-        @author Wei Ren
-        @param
-        @return JSON object of header information (including segments)
+    def getImageData(self):
+        '''Retrive the image data as a two dimensional array.
+        @param - None
+        @return - A 2-D numpy array of pixel values.
         '''
-        pass
-
-    def getImageData(self, segPosition=""):
-        '''Retrive the image data as a two dimensional array. It retrives data
-        of one segment if the FITS is in multi-extension format.
-        @author Wei Ren
-        @param segPosition - String (optional) speicify the "position" of
-                                an extension if it is a multi-extension FITS.On
-                                single-extension FITS this argument is ignored.
-        @return A 2-D numpy array of pixel values.
-        '''
-        if self.isMulti:
-            if not re.match(r"^\d{2}$", segPosition):
-                raise ValueError("Wrong argument segPosition passed in. Expected to be string of two digits, e.g. '17'")
-            hdu = self.__getImageHDUFromHDUList(segPosition)
-            return hdu.data
-        else:
-            return self.primaryHDU.data
-
-    def getSegmentHeader(self, segPosition=""):
-        '''Retrive the header of one segment. It will return the primary header
-        if the FITS image is single-extension.
-        @author Wei Ren
-        @param segPosition - see getImageData
-        @return A pointer to the astropy.fits header object.
-        '''
-        if self.isMulti:
-            if not re.match(r"^\d{2}$", segPosition):
-                raise ValueError("Wrong argument segPosition passed in. Expected to be string of two digits, e.g. '17'")
-            hdu = self.__getImageHDUFromHDUList(segPosition)
-            return hdu.header
-        else:
-            return self.primaryHDU.header
+        return self.primaryHDU.data

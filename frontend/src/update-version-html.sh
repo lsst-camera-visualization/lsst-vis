8e64f3cb8613b43bc2ffeb50a53990c2cc647c94
Wei <victor-ren@outlook.com>
2017-07-21 17:04:26 -0700

    build script automatically update the version html file

--- frontend/src/update-version-html.sh
+++ frontend/src/update-version-html.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 VERSION_FILE="version.html"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 
 # Remove the last line
+cd $DIR
 sed -i '$d' $VERSION_FILE
 # append latest tag
 echo $(git describe --tags --long) >> $VERSION_FILE

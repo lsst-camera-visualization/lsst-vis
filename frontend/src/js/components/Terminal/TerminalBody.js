5095a1732395a3ffe94e0e7717b47b6ca7f2e5bf
Wei <victor-ren@outlook.com>
2017-07-27 10:35:14 -0700

    Open a new tab for the documentation instead of redirecting.

--- frontend/src/js/components/Terminal/TerminalBody.js
+++ frontend/src/js/components/Terminal/TerminalBody.js
@@ -20,7 +20,7 @@ export default class TerminalBody extends React.Component {
                 return <p className="term-body-info" key={i}>{"Info: " + entry.msg}</p>;
             }
             case "LINK": {
-                return <a className="term-body-link" key={i} href={entry.msg.link}>{entry.msg.displayText}</a>;
+                return <a className="term-body-link" key={i} href={entry.msg.link} target="_blank">{entry.msg.displayText}</a>;
             }
 
             default:

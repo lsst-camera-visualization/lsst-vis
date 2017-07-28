4678aeb2aac2a5b3b0eb20a04348118a5bccda40
Wei <victor-ren@outlook.com>
2017-07-28 12:20:05 -0700

    one command per line

--- frontend/src/js/commands/help.js
+++ frontend/src/js/commands/help.js
@@ -1,14 +1,19 @@
+import React from "react";
 import {addInfoToHistory, addLinkToHistory} from "../actions/terminal.actions";
 import {availableCommands} from "./commandDispatcher.js"
 
 import store from "../store";
 
 export const helpCommand = (params) => {
-    const helpPrompt = availableCommands().join(", ");
+    const helpPrompt = "List of available commands:\n"
+    const cmdsLineBreak = availableCommands().map((cmd, keyID) => {
+        return (<p key={keyID}>{cmd}</p>);
+    });
+    const helpInfo = <div>{helpPrompt}{cmdsLineBreak}</div>;
     const helpLink = {
         link: "https://github.com/lsst-camera-visualization/lsst-vis/wiki",
         displayText: "Link to the help documentation"
     };
-    store.dispatch(addInfoToHistory(helpPrompt));
+    store.dispatch(addInfoToHistory(helpInfo));
     store.dispatch(addLinkToHistory(helpLink));
 }
